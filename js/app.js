cd49e0873eb0439dbc681c07dda36870997be982
tongtai <meotai1999@gmail.com>
2022-04-15 12:14:02 +0700

    up code

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$('.customer-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./img/arrow-left.svg"/></button>`,
+  nextArrow: `<button type='button' class='slick-next pull-right'><img src="./img/arrow-right.svg"/></button>`,
+});
