61099add49f5862a6685e44f7e129386bd2f4c82
tongtai <meotai1999@gmail.com>
2022-04-16 07:49:25 +0700

    responsive @1279 @767

--- js/app.js
+++ js/app.js
@@ -5,4 +5,28 @@ $('.customer-list').slick({
   dots: true,
   prevArrow: `<button type='button' class='slick-prev pull-left'><img src="./img/arrow-left.svg"/></button>`,
   nextArrow: `<button type='button' class='slick-next pull-right'><img src="./img/arrow-right.svg"/></button>`,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
 });
